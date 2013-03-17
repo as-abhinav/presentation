39ff42b82b301e5f4a4ae0d63c46c690b7687794
Shridhar Deshmukh <shridhad@thoughtworks.com>
2013-03-17 14:31:19 +0530

    [Shridhar] - Presentation View

--- public/javascripts/views/presentation.js
+++ public/javascripts/views/presentation.js
@@ -1,13 +1,43 @@
 $(function() {
-    var newPresentationDialog = $("#newPresentationDialog")
-    newPresentationDialog.on('submit','form', function(){
+    var appView = Backbone.View.extend({
+
+    });
+
+    var $newPresentationDialog = $("#newPresentationDialog"),
+        presentationTemplate = $("#presentationTemplate").html(),
+        $presentationContainer = $("#presentationContainer");
+
+    $newPresentationDialog.on('submit','form', function(){
         var $form = $(this);
-        app.presentationCollection.push(new app.Presentation({
+        app.createPresentation({
             name: $form.find("#name").val(),
             description: $form.find("#description").val()
-        }));
+        });
         console.log("Created " + app.presentationCollection);
-        newPresentationDialog.modal('hide');
+        $newPresentationDialog.modal('hide');
         return false;
     });
+
+    app.createPresentation = function(obj) {
+        var newPresentation = new app.Presentation(obj);
+        app.presentationCollection.push(newPresentation);
+        $presentationContainer.append(_.template(presentationTemplate, obj))
+
+    };
+
+    app.PresentationView = Backbone.View.extend({
+        tagName: "ul",
+        id: "presentationContainer",
+
+        events : {
+            "click .remove" : "removePresentation"
+        },
+
+        removePresentation : function() {
+          this.model.destroy();
+        }
+    })
+
+
+
 });
