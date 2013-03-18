45ff454202e16217ce57cd8fcc12e13d04191b25
vishnun <vishnun@thoughtworks.com>
2013-03-18 21:51:37 +0530

    Vishnu - AngularSpike - Add new form updated the existing presentation template. Able to use one model for multiple views.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,27 @@
 'use strict';
 
+/*Models*/
+var newPresentation = {
+  title: "",
+  description: ""
+};
+
+
 /* Controllers */
+
+function AddNewPresentationCtrl($scope) {
+  $scope.presentation = newPresentation;
+  $scope.presentation.addNew = function($event){
+    $event.preventDefault();
+    $("#newPresentation")[0].reset();
+    $("#addPresentationDialog").modal('toggle');
+    return false;
+  };
+};
+
+function ListPresentationsCtrl($scope){
+  $scope.presentation = newPresentation;
+};
 
 
 function MyCtrl1() {}
