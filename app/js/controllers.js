29d89714cd4df42e61c556cf492a3a37de4eca10
vishnun <vishnun@thoughtworks.com>
2013-03-18 22:06:58 +0530

    Vishnu - Angular - Add new presentation bindings done first cut.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /*Models*/
+var presentations = [];
 var newPresentation = {
   title: "",
   description: ""
@@ -11,16 +12,23 @@ var newPresentation = {
 
 function AddNewPresentationCtrl($scope) {
   $scope.presentation = newPresentation;
+  $scope.presentations = presentations;
   $scope.presentation.addNew = function($event){
     $event.preventDefault();
     $("#newPresentation")[0].reset();
     $("#addPresentationDialog").modal('toggle');
+//    $("#presentationContainer").append($("#presentationTemplate").html());
+    $scope.presentations.push({
+      title: $scope.presentation.title,
+      description: $scope.presentation.description
+    });
     return false;
   };
+
 };
 
 function ListPresentationsCtrl($scope){
-  $scope.presentation = newPresentation;
+  $scope.presentations = presentations;
 };
 
 
